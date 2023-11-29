9f2d5fbd258ff4d793a39d0de54bf08151bea0a9
Benjamin Klempay <bklempay@ucsd.edu>
2023-11-28 16:37:10 -0800

    minor changes

--- ONF_dgen.R
+++ ONF_dgen.R
@@ -4,30 +4,29 @@ library(stringi)
 
 #### Calculate degenerated oligonucleotide frequencies ####
 
-files <- commandArgs(TRUE) # pass bash input to R
+files <- commandArgs(TRUE) # pass command line arguments to R
 
-for(file in files){
+for(f in files){
+  if(!(file.exists(f) && grepl("\\.(rds|tsv)$", f, ignore.case = TRUE))) stop(paste0("illegal file \'",f,"\'"))
+  
   # Read raw oligonucleotide frequency matrix (e.g. output of `ONF_calc.R`)
-  if(grepl("\\.rds$", file, ignore.case = TRUE)){
-    data <- readRDS(file)
-  } else {
-    data <- data.matrix(read.delim(file, row.names = 1))
-    saveRDS(data, sub("\\.\\w*$", ".rds", file)) # save ONF matrix as RDS file if it isn't already
-  }
+  if(grepl("\\.rds$", f, ignore.case = TRUE)) onf <- readRDS(f)
+  if(grepl("\\.tsv$", f, ignore.case = TRUE)) onf <- data.matrix(read.delim(f, row.names = 1))
+  # assumes row names in first column
   
   # Find column indices of reverse complement for each kmer
-  kmers <- colnames(data)
+  kmers <- tolower(colnames(onf))
   dgen.key <- stri_replace_all(kmers, fixed = c("a","c","g","t"), c("T","G","C","A"), vectorize_all = FALSE)
   dgen.key <- stri_reverse(tolower(dgen.key))
   dgen.key <- match(dgen.key, kmers)
   cols2sum <- !is.na(dgen.key) & dgen.key > 1:length(kmers)
   cols2del <- !is.na(dgen.key) & dgen.key < 1:length(kmers)
   
-  data[,cols2sum] <-  data[,cols2sum] + data[,dgen.key[cols2sum]] # sum kmers + reverse complements
-  data <- data[,!cols2del] # remove reverse complements from matrix
+  onf[,cols2sum] <-  onf[,cols2sum] + onf[,dgen.key[cols2sum]] # sum kmers + reverse complements
+  onf <- onf[,!cols2del] # remove reverse complements from matrix
   # do nothing to kmers that are their own reverse complement
-  data <- data/rowSums(data) # normalize degenerated kmer counts
+  onf <- onf/rowSums(onf) # normalize degenerated kmer counts
   
   # Save degenerated ONF matrix as RDS file
-  saveRDS(data, sub("\\.\\w*$", "_dgen.rds", file))
+  saveRDS(onf, sub("\\.\\w*$", "_dgen.rds", f))
 }
